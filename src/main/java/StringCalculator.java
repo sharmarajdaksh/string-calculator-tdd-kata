f65620feea84ad21148ce06f7fb445780299ebd3
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-08-04 21:53:22 +0530

    First working implementation of the add method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,12 @@
 public class StringCalculator {
-    int add(String numbers) {
-        return 0;
+    public int add(String numbers) throws NumberFormatException {
+        String[] numbersList = numbers.split(",");
+
+        if (numbersList.length < 1) return 0;
+        if (numbersList.length == 1) {
+            if (numbersList[0] == "") return 0;
+            return Integer.parseInt(numbersList[0]);
+        }
+        return Integer.parseInt(numbersList[0]) + Integer.parseInt(numbersList[1]);
     }
 }
