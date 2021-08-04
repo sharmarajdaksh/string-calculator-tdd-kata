021630cfae54646eb7bc5a76ece9d4f9505dd0e2
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-08-04 21:42:12 +0530

    Added testcases for add method

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,5 @@
+public class StringCalculator {
+    int add(String numbers) {
+        return 0;
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class StringCalculatorTest {
+
+    class AddTestCase {
+        private final String input;
+        private final int expected;
+
+        AddTestCase(String input, int expected) {
+            this.input = input;
+            this.expected = expected;
+        }
+
+
+        public String getInput() {
+            return input;
+        }
+
+        public int getExpected() {
+            return expected;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        List<AddTestCase> testCases = new ArrayList<>();
+        testCases.add(new AddTestCase( "", 0));
+        testCases.add(new AddTestCase("0", 0));
+        testCases.add(new AddTestCase("5", 5));
+        testCases.add(new AddTestCase("2,3", 5));
+        testCases.add(new AddTestCase("4,5", 9));
+
+        StringCalculator calculator = new StringCalculator();
+        for (AddTestCase testCase: testCases) {
+            Assertions.assertEquals(calculator.add(testCase.getInput()), testCase.getExpected());
+        }
+    }
+}
